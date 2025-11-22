1bc97aa4dbab08fa9e0afb2881033187b5360cfe
24070-sak <24070@supnum.mr>
2025-11-22 19:10:22 +0000

    Update authService.js

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,1 +1,13 @@
-export const login = () => {};
+import API_URL from "../api";
+
+export async function login(email, password) {
+  const response = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, password })
+  });
+
+  return response.json();
+}
