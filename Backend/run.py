39164f32131c43dacd56f7bfedbecd5a8f9fbcdb
Pwn0r <pwn0r@sak.sak>
2025-11-23 01:53:20 +0000

    merge remote

--- Backend/run.py
+++ Backend/run.py
@@ -1,6 +1,10 @@
 from app import create_app
 
 app = create_app()
+@app.route("/")
+def home():
+    return "Hotely API is running"
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
